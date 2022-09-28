249af78f5d2f5f85c62adce015762c04c9b8708c
Bruno <phasedst7@gmail.com>
2022-09-28 13:37:48 +0200

    Added apartment photos & reorganized html via scripts

---
+++ scripts/images-app/images-app-2.js
@@ -0,0 +1,27 @@
+document.write(
+    `
+    <div class="image second-first zoom intense" data-image="./photos/apartment-2/living-room.jpg"
+        data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+    <div class="image second-second zoom intense" data-image="./photos/apartment-2/living-room-2.jpg"
+        data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+    <div class="image second-third zoom intense" data-image="./photos/apartment-2/living-room-3.jpg"
+        data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+    <div class="image second-fourth zoom intense" data-image="./photos/apartment-2/stairs.jpg"
+        data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+    <div class="image second-fifth zoom intense" data-image="./photos/apartment-2/bedroom.jpg"
+        data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+    <div class="image second-sixth zoom intense" data-image="./photos/apartment-2/bedroom-2.jpg"
+        data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+    <div class="image second-seventh zoom intense" data-image="./photos/apartment-2/bedroom-3.jpg"
+        data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+    <div class="image second-eighth zoom intense" data-image="./photos/apartment-2/toilet.jpg"
+        data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+    `
+);
---
+++ scripts/images-app/images-app-3.js
@@ -0,0 +1,60 @@
+document.write(
+    `
+<div class="image third-first zoom intense" data-image="./photos/apartment-3/dining-room.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-second zoom intense" data-image="./photos/apartment-3/dining-room-2.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-third zoom intense" data-image="./photos/apartment-3/kitchen.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-fourth zoom intense" data-image="./photos/apartment-3/dining-bed.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-fifth zoom intense" data-image="./photos/apartment-3/bed.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-sixth zoom intense" data-image="./photos/apartment-3/couch-tv.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-seventh zoom intense" data-image="./photos/apartment-3/couch-hall.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-eighth zoom intense" data-image="./photos/apartment-3/flower-stand.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-ninth zoom intense" data-image="./photos/apartment-3/terrace-stairs-2.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-tenth zoom intense" data-image="./photos/apartment-3/terrace-stairs.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-eleventh zoom intense" data-image="./photos/apartment-3/terrace.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-twelveth zoom intense" data-image="./photos/apartment-3/chandelier.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-thirteenth zoom intense" data-image="./photos/apartment-3/toilet.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-fourteenth zoom intense" data-image="./photos/apartment-3/toilet-2.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-fifteenth zoom intense" data-image="./photos/apartment-3/shower.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-sixteenth zoom intense" data-image="./photos/apartment-3/sink.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-seventeenth zoom intense" data-image="./photos/apartment-3/wall.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-eighteenth zoom intense" data-image="./photos/apartment-3/stairs.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+
+<div class="image third-nineteenth zoom intense" data-image="./photos/apartment-3/windows-outside.jpg"
+data-title="Lorem ipsum dolor sit amet" data-caption="consectetur adipiscing elit"></div>
+`
+);
